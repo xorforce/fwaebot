5d5a7ffb7d14d0ccae4a5b5fd62342f9b6041792
Bhagat Singh <bhagatsingh2297@gmail.com>
2026-03-21 00:59:45 +0530

    ci: deploy Next.js blog to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "fwaebot";
+
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   experimental: {
     typedRoutes: true
   }
